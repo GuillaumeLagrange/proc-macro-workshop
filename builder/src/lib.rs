f5f4a60081aa21911635c899f5d8c8d96fc915ad
Guillaume Lagrange <guillaume@glagrange.eu>
2025-01-28 16:57:14 +0100

    feat: part 1 & 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,47 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = &input.ident;
+    let builder_ident = format_ident!("{}Builder", ident);
 
-    unimplemented!()
+    let data = input.data;
+
+    let fields = match data {
+        syn::Data::Struct(data) => {
+            let fields = data.fields;
+
+            match fields {
+                Fields::Named(fields) => fields
+                    .named
+                    .into_iter()
+                    .map(|f| (f.ident.unwrap(), f.ty))
+                    .collect::<Vec<_>>(),
+                _ => unimplemented!(),
+            }
+        }
+        _ => unimplemented!(),
+    };
+
+    let field_names = fields.iter().map(|(name, _)| name).collect::<Vec<_>>();
+    let field_types = fields.iter().map(|(_, ty)| ty).collect::<Vec<_>>();
+
+    let expanded = quote! {
+        pub struct #builder_ident {
+            #(#field_names: Option<#field_types>),*
+        }
+
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    #(#field_names: None),*
+                }
+            }
+        }
+    };
+
+    expanded.into()
 }
